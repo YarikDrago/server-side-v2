2bfacba1a2d4f38c3c524506671e7d292959f633
Iaroslav Uliantcev <doudou239@gmail.com>
2022-11-23 22:10:17 +0300

    comment WebSocket. Make universal usage for http and https. Disable work of external request like Contact Form and Football data

--- index.js
+++ index.js
@@ -1,33 +1,45 @@
 require('dotenv').config()
 
 const express = require('express')
+const http = require('http')
+const https = require('https');
+const fs = require('fs');
 const cors = require('cors')
+const path = require('path')
+
 const mailer = require('./nodemailer/nodemailer')
 const googleSheet = require('./googleSheet/googleSheet')
 const transformSheetData = require('./googleSheet/transformSheetData')
 
-
-
 const app = express()
 app.use(express.json())
 app.use(cors())
-const WSServer = require('express-ws')(app)
-const aWss = WSServer.getWss()
 
-const footballChatMsg = ['connected']
+// const directoryToServe = 'client'
+const directoryToServe = process.env.CLIENT_PATH
 
-app.ws('/footballchat', (ws, req)=>{
-    ws.on('message', (msg) => {
-        console.log("msg", msg)
-        // ws.send('yes! yes! I can hear you')
-        // для все подключенных клиентов
-        aWss.clients.forEach(client => {
-            console.log("aws")
-            // client.send('yes! yes! I can hear you')
-            client.send(msg)
-        })
-    })
-})
+// app.use('/', express.static(path.join(__dirname, '..', directoryToServe)))
+app.use('/', express.static(path.join(__dirname, '', directoryToServe)))
+
+
+// const WSServer = require('express-ws')(app)
+// const aWss = WSServer.getWss()
+// const footballChatMsg = ['connected']
+// app.ws('/footballchat', (ws, req)=>{
+//     ws.on('message', (msg) => {
+//         console.log("msg", msg)
+//         // ws.send('yes! yes! I can hear you')
+//         // для все подключенных клиентов
+//         aWss.clients.forEach(client => {
+//             console.log("aws")
+//             // client.send('yes! yes! I can hear you')
+//             client.send(msg)
+//         })
+//     })
+// })
+
+
+
 
 app.get('/test',  async(req, res) => {
     try {
@@ -66,13 +78,29 @@ app.get('/football_data',  async(req, res) => {
 })
 
 
+// const options = {
+//     key: fs.readFileSync('key.pem'),
+//     cert: fs.readFileSync('cert.pem')
+// };
+
 const start = async () => {
-    const PORT = process.env.PORT || 5000
-    try {
-        app.listen(PORT, () => console.log('Server was started on port: ', PORT))
-    } catch (e) {
-        console.log('Starting server error: ', e)
+    const PORT = Number(process.env.PORT || 5000)
+    if (process.env.TP === 'https'){
+        const options = {
+            key: fs.readFileSync('server.key'),
+            cert: fs.readFileSync('server.crt')
+        };
+        const server = https.createServer(options, app)
+        try {
+            // app.listen(PORT, () => console.log('Server was started on port: ', PORT))
+            server.listen(PORT, () => console.log(`Server was started on port: ${PORT} https`))
+        } catch (e) {
+            console.log('Starting server error: ', e)
+        }
+    } else {
+        app.listen(PORT, ()=>{console.log(`server started on port: ${PORT}, http`)})
     }
+
 }
 
 start()
